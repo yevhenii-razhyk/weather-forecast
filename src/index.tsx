6500f348c9b03c656ca4acdbb4b693126be793a6
yevhenii-razhyk <yevhenii-razhyk@outlook.com>
2022-10-18 15:41:11 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react'
 import './styles/index.scss';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import { setupStore } from './store/store';
+import store, {persistor} from './store/store';
 
-const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { 
+    persistStore, 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import currentWeatherReducer from './reducers/CurrentWeatherSlice';
 import city from "./reducers/CitySlice";
 import forecastReducer from "./reducers/ForecastSlice"
@@ -10,12 +21,27 @@ const rootReducer = combineReducers({
     city
 });
 
-export const setupStore = () => {
-    return configureStore({
-        reducer: rootReducer
-    })
+const persistConfig = {
+    key: 'root',
+    storage,
 }
 
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof setupStore>;
-export type AppDispatch = AppStore['dispatch']
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
